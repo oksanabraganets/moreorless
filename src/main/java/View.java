909ceac3a5c503d43b37290c734e996b271fc554
oksana <oksanaov@gmail.com>
2020-02-09 10:00:58 +0200

    View class created

---
+++ src/main/java/View.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class View {
+    public static final String INPUT_INT_DATA = "Input INT value from %d to %d %n";
+    public static final String WRONG_INPUT_INT_DATA = "Wrong input! Repeat please! ";
+
+    public void printMessage(String message){
+        System.out.println(message);
+    }
+
+    public void printMessageInRange(int boundLow, int boundHi){
+        System.out.printf(INPUT_INT_DATA, boundLow, boundHi);
+    }
+
+    public void printStatistics(ArrayList<Integer> statistics){
+        for(int i=0; i<statistics.size(); i++)
+            System.out.print(statistics.get(i) + " ");
+    }
+}
